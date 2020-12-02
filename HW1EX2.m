ac5820fba3342fe8bae3d6bf2922c5f649436f86
Jung Kim <74718951+jwkim415@users.noreply.github.com>
2020-12-02 10:45:27 +0900

    Add files via upload
    Newton's law of universal gravitation
    program that calculates the gravitational force between two objects. 
    Force = G*(m1m2)/d^2

---
+++ HW1EX2.m
@@ -0,0 +1,11 @@
+%{
+  Jungwon Kim
+  Homework 1
+  Exercise 1
+%}
+MassOfTheFirstObject = input ('Enter The mass of the first object: '); %mass is in kilogram
+MassOfTheSecondObject = input ('Enter The mass of the second object: '); %mass is in kilogram
+TheDistanceBetweenTheTwoObjects = input ('Enter The distance between the two objects: '); %distance is in meters
+G = 6.67*10^-11;
+Force = (G*MassOfTheFirstObject*MassOfTheSecondObject)/(TheDistanceBetweenTheTwoObjects)^2;
+fprintf('Force = %.3e \n', Force )
